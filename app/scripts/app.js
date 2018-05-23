196758a8650d23830641fe533179060d8fde82db
camwhaler <camwhaler@gmail.com>
2018-05-23 14:46:48 -0500

    Some big fixes
    Back button fixed and calendar corrected!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,4 @@
-﻿/*!
- * This file is part of App Builder
- * For licenses information see App Builder help
- * ©2018 App Builder - https://www.davidesperalta.com
- */
-
+﻿
 window.App = {};
 
 window.App.Utils = (function () {
